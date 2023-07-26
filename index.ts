2dae6139b3ef79155654848cb954b0366a665ad3
Manoj Ranga <manojranga7583@gmail.com>
2023-07-26 22:26:53 +0530

    assignment 2: update 1.0

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-//Assignment 1
+import * as readlineSync from 'readline-sync';
 
+//Assignment 1
+/*
 let userName: string = "TypeScript User";
 
 let repetitions: number = 5;
@@ -10,4 +12,44 @@ function greetUser(name: string, times: number) {
 }
 
 greetUser(userName, repetitions);
+*/
 
+type Question = {
+    question: string,
+    options: string[],
+    answer: string
+};
+const questions: Question[] = [
+    {
+        question: "What is TypeScript?",
+        options: ["A programming language", "A movie", "A type of pseudocode"],
+        answer: "A programming language"
+    },
+    {   question: "Who created TypeScript?",
+        options: ["Microsoft", "Apple", "Google"],
+        answer: "Microsoft"
+    }
+];
+
+function askQuestion(question: Question): string {
+    console.log(question.question);
+    let index = readlineSync.keyInSelect(question.options, "Please choose your answer:");
+    if (index === -1) {
+        console.log("You chose to skip the question.");
+        return "";
+    }
+    return question.options[index];
+}
+
+function runQuiz(questions: Question[]) {
+    let score = 0;
+    for (let question of questions) {
+        let answer = askQuestion(question);
+        if (answer === question.answer) {
+            score++;
+        }
+    }
+    console.log(`Your score is: ${score}/${questions.length}`);
+}
+
+runQuiz(questions);
